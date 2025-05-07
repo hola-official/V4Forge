798ffae69290202c5b2ebb18637231e54e396e35
Muhammed Musa <sam@overthetopseo.com>
2025-05-07 22:34:28 +0100

    wallet config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { WagmiConfigProvider } from './providers/Wagmi.jsx'
+import "@rainbow-me/rainbowkit/styles.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WagmiConfigProvider>
+      <App />
+    </WagmiConfigProvider>
   </StrictMode>,
 )
